862d0405b2263fdb38d0ce9354084db0cb5ac8bc
abdel-lah <abdellahouazane2002@gmail.com>
2022-12-31 17:28:05 +0100

    ADD First Module in Sequelize
    start up Sequelize
    in create first modul (product)

--- models/products.js
+++ models/products.js
@@ -1,34 +1,28 @@
-const Sequelize=require('sequelize');
+const Sequelize = require("sequelize");
+const sequelize = require("../util/database");
 
-const sequelize=require("../util/database");
+const Product = sequelize.define("product", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
 
-const Product=sequelize.
-
-// const Cart = require("../modules/cart");
-// const db = require("../util/database");
-// module.exports = class Product {
-//   constructor(id, title, imgaurl, price, description) {
-//     this.id = id;
-//     this.title = title;
-//     this.imgaurl = imgaurl;
-//     this.description = description;
-//     this.price = price;
-//   }
-//   save() {
-//     return db.execute(
-//       "Insert into products (title,price,description,imageurl) values(?,?,?,?)",
-//       [this.title, this.price, this.description, this.imgaurl]
-//     );
-//   }
-
-//   static fetchall() {
-//     return db.execute("Select * from products");
-//   }
-
-//   static findbyid(PID) {
-//     return db.execute("select * from products where id=? ", [PID]);
-//   }
-//   static deletebyid(id) {
-
-//   }
-// };
+module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
-const Sequelize = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-// const sequelize = new Sequelize("new_schema", "root", "nodecomplete", {
-//   dialect: "mysql",
-//   host: "localhost",
-// });
+const sequelize = new Sequelize("new_schema", "root", "nodecomplete", {
+  dialect: "mysql",
+  host: "localhost",
+});
 
 module.exports = sequelize;
